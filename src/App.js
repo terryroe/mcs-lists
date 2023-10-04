a981a339db74988692b545ac5c3def9ecad52a1f
Terry Roe <terryroe@gmail.com>
2023-10-04 12:06:50 -0600

    set up react-bootstrap, bootstrap, react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Container from 'react-bootstrap/Container';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Lists from './components/Lists';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Container>
+        <h1>MCS Lists</h1>
+        <Routes>
+          <Route path="/" element={<Lists />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
